3086da6536dc97a4400a659602da366a1308c86f
Damien Edward Blasko <blaskode@gmail.com>
2020-02-16 09:22:09 -0500

    added push

--- stack.c
+++ stack.c
@@ -10,10 +10,13 @@ struct CharNode {
 link initialize_node(char ele, link lnk);
 void print_list(link head);
 void delete_list(link head);
+link push(link head, link new_node);
 
 int main(void){
 	struct CharNode * node1 = initialize_node('a', NULL);
 	struct CharNode * head = initialize_node('D', node1);
+	struct CharNode * tail = initialize_node('m', NULL);
+	push(head, tail);
 	print_list(head);
 	delete_list(head);
 	return EXIT_SUCCESS;
@@ -37,7 +40,20 @@ void print_list(link head) {
 	}
 }
 
+link push(link head, link new_node) {
+	if (head->next != NULL) {
+		push(head->next, new_node);
+		return NULL;	//this should never be reached
+	} else {
+		head->next = new_node;
+		return new_node;
+	}
+}
+
 void delete_list(link head) {
+	/* There are method calls after the recursive call in */
+	/* the first branch... */
+	/* So, this is not tail-recursion. */
 	if (head->next != NULL) {
 		delete_list(head->next);
 		free(head);
