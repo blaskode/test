1a2266dfd4c9570c919f9294d6d3e89f02351203
Damien Edward Blasko <blaskode@gmail.com>
2020-02-23 13:37:13 -0500

    trying to fix segfault

--- determinant.c
+++ determinant.c
@@ -1,28 +1,7 @@
 #include <stdlib.h>
-#include <stdio.h>
 
-
-int main(int argc, char** argv){
-  int ** matrix;
-  int sz;
-  sz = atoi(argv[1]);
-  matrix = malloc(sz * sizeof(char *));
-  int row = 0;
-  int col = 0;
-
-  FILE * f = fopen(argv[2], "r");
-  int c = getc(f);
-  matrix[row][col] = c;
-  printf("%d", c);
-  col++;
-  while(c != EOF){
-    matrix[row][col] = c;
-    printf("%d", c);
-    if (c == '\n'){
-      row++;
-      col = 0;
-    } else {
-      col++;
-    }
-  }
+int main(void){
+  int ** matrix = malloc(3 * sizeof(int *));
+  matrix[0] = malloc(3 * sizeof(int));
+  matrix[0][0] = 1;
 }
