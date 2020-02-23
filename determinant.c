7f47bd64454442cfba1a5dc86ac5efe2ec0391c1
Damien Edward Blasko <blaskode@gmail.com>
2020-02-23 13:40:33 -0500

    trying to fix segfault

--- determinant.c
+++ determinant.c
@@ -1,7 +1,9 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 int main(void){
   int ** matrix = malloc(3 * sizeof(int *));
   matrix[0] = malloc(3 * sizeof(int));
   matrix[0][0] = 1;
+  printf("%d", matrix[0][0]);
 }
