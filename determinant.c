09f2bc28e8b02dfa5bd61b9ba09dca8a4f6c1fac
Damien Edward Blasko <blaskode@gmail.com>
2020-02-24 08:48:52 -0500

    starting over

--- determinant.c
+++ determinant.c
@@ -2,56 +2,13 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, char** argv){
-  if (atoi(argv[1]) == 2){
-    int ** matrix = malloc(2 * sizeof(int *));
-    matrix[0] = malloc(2 * sizeof(int));
-    matrix[1] = malloc(2 * sizeof(int));
-    FILE * f = fopen(argv[2], "r");
-    int c = fgetc(f);
-    //c = atoi(c);
-    int i, j;
-    i = j = 0;
-    while(c != EOF){
-      //if (i > 1 || j > 1){
-        //break;
-      //}
+int main(){
+  int ** matrix = malloc(2 * sizeof(int *));
+  matrix[0] = malloc(2 * sizeof(int));
+  matrix[1] = malloc(2 * sizeof(int));
 
-      if (c == 32) {
-        c = fgetc(f);
-        continue;
-      }
-      if (c == '\n'){
-        c = c - 48;
-        if (i < 2 || j < 2){
-          matrix[i][j] = c;
-        }
-        i++;
-        j = 0;
-      } else {
-        if ( i < 2 || j < 2){
-         c = c - 48;
-        }
-        matrix[i][j] = c;
-        j++;
-      }
-      c = fgetc(f);
-      printf("%d %d\n", i, j);
-    }
-    int a = matrix[0][0]; 
-    int b = matrix[1][1]; 
-    int e = matrix[1][0];
-    int d = matrix[0][1];
-    int det = a * b - e * d;
-    printf("Determinant: %d\n", det);
-
-    for(int i = 0; i < 2; i++){
-      free(matrix[i]);
-    }
-    free(matrix);
-
-  } else {
-    printf("Wrong size of matrix.\n");
+  for(int i = 0; i < 2; i++){
+    free(matrix[i]);
   }
-
+  free(matrix);
 }
