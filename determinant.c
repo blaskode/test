1fb1cf5cbd968cc546241f0b6aa65e10e820013a
Damien Edward Blasko <blaskode@gmail.com>
2020-02-24 09:52:18 -0500

    made determinant function

--- determinant.c
+++ determinant.c
@@ -3,48 +3,18 @@
 #include <string.h>
 
 void read_matrix(FILE * f, int sz, int ** matrix);
+int determinant(int ** matrix);
 void free_matrix(int ** matrix, int sz);
 
 int main(int argc, char ** argv){
   int ** matrix = malloc(2 * sizeof(int *));
   matrix[0] = malloc(2 * sizeof(int));
   matrix[1] = malloc(2 * sizeof(int));
-
   FILE * f = fopen(argv[1], "r");
-  read_matrix(f, 2, matrix);  //
-  /*
-  int e = getc(f);
-  int i, j;
-  i = j = 0;
-  while (e != EOF){
-    if (e == 32){ //space
-      e = getc(f);
-    } else if (e == 10){ //newline
-      e = getc(f);
-      i++;
-      j = 0;
-    } else { //data we can use
-      e = e - 49;
-      matrix[i][j] = e;
-      //printf("%d ", e);
-      //printf("%d %d ", i, j);
-      e = getc(f);
-      j++;
-    }
-  }
-  int a, b, c, d;
-  a = matrix[0][0];
-  b = matrix[1][1];
-  c = matrix[0][1];
-  d = matrix[1][0];
-  int det = a*b - c*d;
+  read_matrix(f, 2, matrix);  
+  int det = determinant(matrix);
   printf("Determinant: %d\n", det);
-  */
   fclose(f);
-
-  //for(int i = 0; i < 2; i++){
-    //free(matrix[i]);
-  //}
   free_matrix(matrix, 2);
 }
 
@@ -69,17 +39,18 @@ void read_matrix(FILE * f, int sz, int ** matrix){
     } else { //data we can use
       e = e - 49;
       matrix[i][j] = e;
-      //printf("%d ", e);
-      //printf("%d %d ", i, j);
       e = getc(f);
       j++;
     }
   }
+}
+
+int determinant(int ** matrix){
   int a, b, c, d;
   a = matrix[0][0];
   b = matrix[1][1];
   c = matrix[0][1];
   d = matrix[1][0];
   int det = a*b - c*d;
-  printf("Determinant: %d\n", det);
+  return det;
 }
