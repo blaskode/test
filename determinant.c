bad6f335f171fff23b0f7385a90227a1b3ff37fc
Damien Edward Blasko <blaskode@gmail.com>
2020-02-24 19:19:40 -0500

    adjusetd for 3x3

--- determinant.c
+++ determinant.c
@@ -3,19 +3,20 @@
 #include <string.h>
 
 void read_matrix(FILE * f, int sz, int ** matrix);
-int determinant(int ** matrix);
+int determinant(int ** matrix, int n);
 void free_matrix(int ** matrix, int sz);
 
 int main(int argc, char ** argv){
-  int ** matrix = malloc(2 * sizeof(int *));
-  matrix[0] = malloc(2 * sizeof(int));
-  matrix[1] = malloc(2 * sizeof(int));
+  int n = atoi(argv[2]);
+  int ** matrix = malloc(n * sizeof(int *));
+  matrix[0] = malloc(n * sizeof(int));
+  matrix[1] = malloc(n * sizeof(int));
   FILE * f = fopen(argv[1], "r");
-  read_matrix(f, 2, matrix);  
-  int det = determinant(matrix);
+  read_matrix(f, n, matrix);  
+  int det = determinant(matrix, n);
   printf("Determinant: %d\n", det);
   fclose(f);
-  free_matrix(matrix, 2);
+  free_matrix(matrix, n);
 }
 
 void free_matrix(int ** matrix, int sz){
@@ -25,32 +26,40 @@ void free_matrix(int ** matrix, int sz){
   free(matrix);
 }
 
+
 void read_matrix(FILE * f, int sz, int ** matrix){
-  int e = getc(f);
-  int i, j;
-  i = j = 0;
-  while (e != EOF){
-    if (e == 32){ //space
-      e = getc(f);
-    } else if (e == 10){ //newline
-      e = getc(f);
-      i++;
-      j = 0;
-    } else { //data we can use
-      e = e - 49;
-      matrix[i][j] = e;
-      e = getc(f);
-      j++;
+  int n;
+  for(int i = 0; i < sz; i++){
+    for(int j = 0; j< sz; j++){
+      fscanf(f, "%d", &n);
+      matrix[i][j] = n;
     }
   }
 }
 
-int determinant(int ** matrix){
-  int a, b, c, d;
-  a = matrix[0][0];
-  b = matrix[1][1];
-  c = matrix[0][1];
-  d = matrix[1][0];
-  int det = a*b - c*d;
-  return det;
+int determinant(int ** matrix, int n){
+  if (n == 2){
+    int a, b, c, d;
+    a = matrix[0][0];
+    b = matrix[1][1];
+    c = matrix[0][1];
+    d = matrix[1][0];
+    int det = a*b - c*d;
+    return det;
+  } else if (n == 3){
+    int a, b, c, d, e, f, g, h, i;
+    a = matrix[0][0];
+    b = matrix[0][1];
+    c = matrix[0][2];
+    d = matrix[1][0];
+    e = matrix[1][1];
+    f = matrix[1][2];
+    g = matrix[2][0];
+    h = matrix[2][1];
+    i = matrix[2][2];
+    int det;
+    det = a*(e*i - f*h) - b*(d*i - f*g) + c*(d*h - e*g);
+    return det;
+  }
+  return 0;
 }
